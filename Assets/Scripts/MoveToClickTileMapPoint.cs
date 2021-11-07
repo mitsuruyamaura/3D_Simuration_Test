d7178ad5fa803402e7e57af424e78e50f965e6c1
mitsuru_yamaura <komayou326@gmail.com>
2021-11-07 14:35:27 +0900

    キャラ作成。スプライトアニメ作成。MoveToClickTileMapPoint.cs 作成。Bake してある地点をクリックすると、キャラが障害物を避けて目標地点までナビ移動する機能の実装。 デバッグ開始。

---
+++ Assets/Scripts/MoveToClickTileMapPoint.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.AI;
+
+//【Unityで2DRPG】NPCをNavMeshAgentで動かしてみる (猫の冒険)
+//https://a1026302.hatenablog.com/entry/2020/11/21/014727
+public class MoveToClickTileMapPoint : MonoBehaviour {
+
+    [SerializeField]
+    private Grid grid;
+
+    [SerializeField]
+    private Tilemap tilemap;
+
+    private NavMeshAgent agent;
+
+    void Start() {
+
+        if (TryGetComponent(out agent)) {
+
+            // インスタンス対応。事前に入れておくと、Bake している地点を認識できずにエラーになるため
+            agent.enabled = true;
+
+            // 2D なので、これがないと、変な位置に勝手に移動する
+            agent.updateRotation = false;
+            agent.updateUpAxis = false;
+
+            // 初期目的地設定(これがないと初期位置からズレる)
+            agent.destination = transform.position;
+        }
+    }
+
+    void Update() {
+
+        // NavMeshAgent が利用できる状態でタップ(マウスクリック)したら
+        if (agent && Input.GetMouseButtonDown(0)) {
+
+            // タップの位置を取得してワールド座標に変換し、それをさらにタイルのセル座標に変換
+            Vector3Int gridPos = grid.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+
+            Debug.Log(gridPos);
+
+            // タップしたタイルが移動不可のタイルでなければ
+            if (tilemap.GetColliderType(gridPos) != Tile.ColliderType.None) {
+
+                // タップしたタイルマップを目的地として移動
+                SetPathAndMove(gridPos);
+            }
+        }
+
+        //// 対象が動く場合は、他でやる
+        //float now = Vector2.Distance(transform.position, nextPos);
+
+        //if (nextPos.magnitude < now) {
+        //    // 再計算
+        //}
+    }
+
+    /// <summary>
+    /// タップ(マウスクリック)したタイルマップを目的地として移動
+    /// </summary>
+    /// <param name="gridPos"></param>
+    private void SetPathAndMove(Vector3Int gridPos) {
+
+        // 目的地点の設定。調整することで、タイルの中央に移動させる
+        Vector2 nextPos = new Vector2(gridPos.x + 0.5f, gridPos.y + 0.5f);
+
+        Debug.Log(nextPos);
+
+        // 目的地の更新
+        agent.destination = nextPos;
+
+        Debug.Log("移動");
+    }
+}
