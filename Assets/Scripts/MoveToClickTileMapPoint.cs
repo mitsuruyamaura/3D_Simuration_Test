faba7e1ccb766eb07b3b68534e6852eced312c37
mitsuru_yamaura <komayou326@gmail.com>
2021-11-09 16:30:51 +0900

    MoveToClickTilemapPoint.cs と CharaController.cs スクリプトを修正して、経路の情報を元にキャラの移動とスプライトアニメとを連動させる機能を実装。 デバッグ開始。

--- Assets/Scripts/MoveToClickTileMapPoint.cs
+++ Assets/Scripts/MoveToClickTileMapPoint.cs
@@ -30,7 +30,7 @@ public class MoveToClickTilemapPoint : MonoBehaviour {
     private DrawPath drawPathPrefab;
 
     private CharaController charaController;
-    //private IEnumerator coroutine;
+    private IEnumerator coroutine;
 
 
     void Start() {
@@ -147,14 +147,19 @@ public class MoveToClickTilemapPoint : MonoBehaviour {
                 // 経路の座標情報取得
                 Vector3[] corners = path.corners;
 
-                //if (coroutine != null) {
-                //    StopCoroutine(coroutine);
-                //    coroutine = null;
-                //}
+                // すでに移動中のアニメを再生している場合
+                if (coroutine != null) {
 
-                //coroutine = charaController.SetAnime(corners);
+                    // アニメ再生を停止
+                    StopCoroutine(coroutine);
+                    coroutine = null;
+                }
 
-                //StartCoroutine(coroutine);
+                // アニメ再生用のメソッド登録
+                coroutine = charaController.SetAnime(corners);
+
+                // 登録したアニメ再生用のメソッドを実行
+                StartCoroutine(coroutine);
 
                 //path.GetCornersNonAlloc(corners);
                 Debug.Log("GetCornersNonAlloc : " + path.GetCornersNonAlloc(corners));  // 最初の地点もいれて数える
